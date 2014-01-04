aa56b234cbe547e80b9c417235ef2e83945dbdba
Julian Squires <julian@cipht.net>
2014-01-03 23:16:29 -0500

    Read perf counter on x86-64

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CLANG ?= clang
 CFLAGS := $(CFLAGS) -Iinclude -Wall -Wextra -Werror -fstrict-aliasing -Wstrict-aliasing -fms-extensions -std=gnu11 -g -O3
-LDFLAGS := $(LDFLAGS) -ltap
+LDFLAGS := $(LDFLAGS) -ltap -lm
 
 # Alternate contestants might include the same ones with different compiler arguments
 CONTESTANTS = minimal-stack-swap ucontext
@@ -13,7 +13,7 @@ all: $(CONTESTANTS:%=t/%.t)
 
 %/..: ; mkdir -p $(@D)
 
-t/%.t: %/*.[cS] tests/harness.c $(TESTS:%=tests/%.c) | t/..
+t/%.t: %/*.[cS] tests/harness.c tests/perf_counter_x86_64.S $(TESTS:%=tests/%.c) | t/.. Makefile
 	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
 
 %.o: %.c
--- tests/harness.c
+++ tests/harness.c
@@ -2,14 +2,42 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 #include "harness.h"
 
 struct test *tests = NULL;
 unsigned int n_tests = 0;
 
+extern uint64_t read_perf_counter(void);
+
+uint64_t determine_perf_counter_overhead(void)
+{
+    enum { N = 1000 };
+    uint64_t v[N], sum = 0;
+    for (int i = 0; i < N; ++i) {
+        uint64_t a = read_perf_counter();
+        uint64_t b = read_perf_counter();
+        v[i] = b-a;
+        sum += b-a;
+    }
+    double mean = (double)sum / N, variance = 0.;
+    for (int i = 0; i < N; ++i) {
+        variance += pow(mean - v[i], 2.);
+    }
+    double sd = sqrt(variance / (N-1));
+    int within = 0;
+    for (int i = 0; i < N; ++i) {
+        within += abs(v[i] - mean) < 1.96*sd ? 1 : 0;
+    }
+    note("performance counter overhead: mean %g, SD %g, %% within: %g\n", mean, sd, 100.*(double)within/N);
+    return mean;
+}
+
 int main(void)
 {
+    uint64_t perf_counter_overhead = determine_perf_counter_overhead();
+
     int expected = 0;
     for (struct test *p = tests; NULL != p; p = p->next) {
         expected += p->expected;
@@ -17,7 +45,10 @@ int main(void)
     plan(expected);
     for (struct test *p = tests; NULL != p; p = p->next) {
         note(p->name);
+        uint64_t then = read_perf_counter();
         p->fn();
+        uint64_t now = read_perf_counter();
+        note("cycles elapsed: %ld", now - then - perf_counter_overhead);
     }
     done_testing();
 }
